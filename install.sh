7fb86cc130fcebfccf53ecba5e3174a2239dc140
Niteshd05 <160988331+Niteshd05@users.noreply.github.com>
2025-10-15 19:10:12 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install --upgrade pip setuptools wheel
+pip install streamlit==1.38.0 pyttsx3==2.90 Pillow==10.4.0
+pip install roboflow==1.1.29 --no-deps
+pip install opencv-python-headless==4.8.0.76
